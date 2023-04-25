4bcd33d7967c63bc6e7839975e1514ef43259b78
Cadu <cdauk@hotmail.com>
2023-04-25 10:09:40 -0300

    refactor: changing validationBook method to validateBook middleware

---
+++ src/middlewares/validateBook.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+
+const validateBook = (req: Request, res: Response, next: NextFunction): void => {
+  const book = req.body;
+  
+  const requiredkeys = ['id', 'title', 'price', 'author', 'isbn']
+
+  requiredkeys.forEach((key) => {
+    if (!(key in book)) return res.status(400).json({ message: `${key} is required` })
+  })
+
+  next()
+}
+
+export default validateBook;
