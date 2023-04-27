e2225396d6582949e01aa8b470abe0df9d3395e7
pedroygor <pedroygor2010@hotmail.com>
2023-04-27 11:01:41 -0300

    test: mock de user

---
+++ tests/mocks/User.mocks.ts
@@ -0,0 +1,18 @@
+const user = {
+    id: 1,
+    name: 'Jon Doe',
+    email: 'jondoe@email.com',
+    password: 'JonDoe'
+};
+
+const users = [
+    user,
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'janedoe@email.com',
+        password: 'JaneDoe'
+    }
+];
+
+export { user, users };
