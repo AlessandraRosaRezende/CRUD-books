f077a2c3e786fbd5a3ba6b5b2611a4d3d86561c1
Cadu <cdauk@hotmail.com>
2023-04-27 10:27:29 -0300

    refactor: transforming sign and verify into static methods

--- src/utils/JWT.ts
+++ src/utils/JWT.ts
@@ -1,28 +1,18 @@
 import { JsonWebTokenError, JwtPayload, Secret, sign, SignOptions, verify } from 'jsonwebtoken';
 import 'dotenv/config';
 
-interface IJWT {
-  sign(payload: JwtPayload): string
-  verify(token: string): JwtPayload
-}
+export default class JWT {
+  private static secret: Secret = process.env.JWT_SECRET || '';
+  private static jwtConfig: SignOptions = {
+    expiresIn: '10d',
+    algorithm: 'HS256',
+  };
 
-export default class JWT implements IJWT {
-  private secret: Secret;
-  private jwtConfig: SignOptions;
-
-  constructor() {
-    this.secret = process.env.JWT_SECRET || '';
-    this.jwtConfig = {
-      expiresIn: '10d',
-      algorithm: 'HS256',
-    };
-  }
-
-  sign(payload: JwtPayload): string {
+  static sign(payload: JwtPayload): string {
     return sign({ ...payload }, this.secret, this.jwtConfig);
   }
 
-  verify(token: string): JwtPayload {
+  static verify(token: string): JwtPayload {
     try {
       const payload = verify(token, this.secret);
       return payload as JwtPayload;
