3c5c81ac6a02b6e1155b7fbf7318ccc6b01eca3e
pedroygor <pedroygor2010@hotmail.com>
2023-04-27 11:26:13 -0300

    remove testes unitários

--- tests/integration/User.test.ts
+++ tests/integration/User.test.ts
@@ -7,11 +7,6 @@ import { users, user } from '../mocks/User.mocks';
 import chaiHttp = require('chai-http');
 
 import SequelizeUser from '../../src/database/models/SequelizeUser';
-import UserModel from '../../src/models/UserModel';
-import UserService from '../../src/services/UserService';
-import JWT from '../../src/utils/JWT';
-import { IUser } from '../../src/interfaces/users/IUser';
-import { NewEntity } from '../../src/interfaces/ICRUDModel';
 
 chai.use(chaiHttp);
 
@@ -20,15 +15,6 @@ const { expect } = chai;
 const { app } = new App();
 
 describe('User', function () {
-  let userService: UserService;
-  let userModel: UserModel;
-  let jwtService: JWT;
-
-  beforeEach(() => {
-    userModel = new UserModel();
-    jwtService = new JWT();
-    userService = new UserService(userModel, jwtService);
-  });
 
   describe('findAll', () => {
     it('should return a list of users', async function () {
@@ -55,80 +41,11 @@ describe('User', function () {
       expect(status).to.equal(200);
       expect(body).to.deep.equal(user);
 
-      expect(findByIdStub.calledOnce).to.be.true;
+      sinon.assert.calledOnce(findByIdStub as any);
 
       sinon.restore();
-    });
-  });
-
-  describe('findOne', () => {
-    it('should return user with email', async () => {
-      const findByEmailStub = sinon.stub(userModel, 'findByEmail').resolves(user);
-
-      const result = await userService.findOne('jondoe@email.com');
-
-      expect(result).to.deep.equal({ status: 'successful', data: user });
-      expect(findByEmailStub.calledOnce).to.be.true;
-    });
-  });
-
-  describe('login', () => {
-    it('should return an error message', async () => {
-      const findByEmailStub = sinon.stub(userModel, 'findByEmail').resolves(user);
-
-      const result = await userService.login({ email: user.email, password: 'wrongPassword' });
-
-      expect(result).to.deep.equal({ status: 'invalidData', data: { message: 'Invalid email or password' } });
-
-      expect(findByEmailStub.calledOnce).to.be.true;
-
-      sinon.restore();
-    });
-
-    it('should return an error message', async () => {
-
-      const findByEmailStub = sinon.stub(userModel, 'findByEmail').resolves(null);
-
-      const result = await userService.login({ email: user.email, password: user.password });
-
-      expect(result).to.deep.equal({ status: 'notFound', data: { message: 'User not found' } });
-
-      expect(findByEmailStub.calledOnce).to.be.true;
-
-      sinon.restore();
-
-    });
-
-    describe('createUser', () => {
-
-      it('should return a new user', async () => {
-        const findByEmailStub = sinon.stub(userModel, 'findByEmail').resolves(null);
-        const createStub = sinon.stub(userModel, 'create').resolves(user);
-
-        const result = await userService.createUser(user as NewEntity<IUser>);
-
-        expect(result).to.deep.equal({ status: 'successful', data: user });
-
-        expect(findByEmailStub.calledOnce).to.be.true;
-
-        expect(createStub.calledOnce).to.be.true;
-
-        sinon.restore();
-      });
-
-      it('should return an error message', async () => {
-        const findByEmailStub = sinon.stub(userModel, 'findByEmail').resolves(user);
-
-        const result = await userService.createUser(user as NewEntity<IUser>);
-
-        expect(result).to.deep.equal({ status: 'conflict', data: { message: 'User already exists' } });
-
-        expect(findByEmailStub.calledOnce).to.be.true;
-
-        sinon.restore();
-      });
-
     });
     afterEach(sinon.restore);
   });
+  
 });
