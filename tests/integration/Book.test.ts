2be16772a581fd4e61cb7ed5404d24f2e7d7b63f
Cadu <cdauk@hotmail.com>
2023-04-27 09:24:09 -0300

    test: test find book by id

--- tests/integration/Book.test.ts
+++ tests/integration/Book.test.ts
@@ -6,6 +6,7 @@ import chaiHttp = require('chai-http');
 
 import App from '../../src/app';
 import SequelizeBook from '../../src/database/models/SequelizeBook';
+import { books, book } from '..//mocks/Book.mocks';
 
 chai.use(chaiHttp);
 
@@ -15,14 +16,21 @@ const { expect } = chai;
 
 describe('Books Test', function () {
   it('should return all books', async function () {
-    sinon.stub(SequelizeBook, 'findAll').resolves(
-      [{ id: 1, title: 'Senhor dos Anéis', price: 22, author: 'Sei não', isbn: '234234' }]  as any
-    );
+    sinon.stub(SequelizeBook, 'findAll').resolves(books as any);
 
     const { status, body } = await chai.request(app).get('/books');
 
     expect(status).to.equal(200);
-    expect(body).to.deep.equal([{ id: 1, title: 'Senhor dos Anéis', price: 22, author: 'Sei não', isbn: '234234' }]);
+    expect(body).to.deep.equal(books);
+  });
+
+  it('should return a book by id', async function () {
+    sinon.stub(SequelizeBook, 'findOne').resolves(book as any);
+
+    const { status, body } = await chai.request(app).get('/books/1');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(book);
   });
 
   afterEach(sinon.restore);
