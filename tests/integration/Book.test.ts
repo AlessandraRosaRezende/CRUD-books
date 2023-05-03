1841ada5ccc5683f488b25c9d4c6906448187a36
Jonathan Ferreira <j0n4t@hotmail.com>
2023-05-03 11:46:09 -0300

    fix: mais fixes agora vai

--- tests/integration/Book.test.ts
+++ tests/integration/Book.test.ts
@@ -1,14 +1,14 @@
-import * as sinon from "sinon";
-import * as chai from "chai";
+import * as sinon from 'sinon';
+import * as chai from 'chai';
 
 // @ts-ignore
-import chaiHttp = require("chai-http");
+import chaiHttp = require('chai-http');
 
-import App from "../../src/App";
-import SequelizeBook from "../../src/database/models/SequelizeBook";
-import { books, book } from "../mocks/Book.mocks";
-import JWT from "../../src/utils/JWT";
-import Validations from "../../src/middlewares/Validations";
+import App from '../../src/App';
+import SequelizeBook from '../../src/database/models/SequelizeBook';
+import { books, book } from '../mocks/Book.mocks';
+import JWT from '../../src/utils/JWT';
+import Validations from '../../src/middlewares/Validations';
 
 chai.use(chaiHttp);
 
@@ -16,209 +16,209 @@ const { app } = new App();
 
 const { expect } = chai;
 
-describe("Books Test", function () {
-  it("should return all books", async function () {
-    sinon.stub(SequelizeBook, "findAll").resolves(books as any);
+describe('Books Test', function() {
+  it('should return all books', async function() {
+    sinon.stub(SequelizeBook, 'findAll').resolves(books as any);
 
-    const { status, body } = await chai.request(app).get("/books");
+    const { status, body } = await chai.request(app).get('/books');
 
     expect(status).to.equal(200);
     expect(body).to.deep.equal(books);
   });
 
-  it("should return a book by id", async function () {
-    sinon.stub(SequelizeBook, "findOne").resolves(book as any);
+  it('should return a book by id', async function() {
+    sinon.stub(SequelizeBook, 'findOne').resolves(book as any);
 
-    const { status, body } = await chai.request(app).get("/books/1");
+    const { status, body } = await chai.request(app).get('/books/1');
 
     expect(status).to.equal(200);
     expect(body).to.deep.equal(book);
   });
 
-  it("should return not found if the book doesn't exists", async function () {
-    sinon.stub(SequelizeBook, "findOne").resolves(null);
+  it('should return not found if the book doesn\'t exists', async function() {
+    sinon.stub(SequelizeBook, 'findOne').resolves(null);
 
-    const { status, body } = await chai.request(app).get("/books/1");
+    const { status, body } = await chai.request(app).get('/books/1');
 
     expect(status).to.equal(404);
-    expect(body.message).to.equal("Book 1 not found");
+    expect(body.message).to.equal('Book 1 not found');
   });
 
-  it("should create a book", async function () {
-    sinon.stub(SequelizeBook, "create").resolves(book as any);
+  it('should create a book', async function() {
+    sinon.stub(SequelizeBook, 'create').resolves(book as any);
 
-    sinon.stub(JWT, "verify").resolves();
-    sinon.stub(Validations, "validateBook").returns();
+    sinon.stub(JWT, 'verify').resolves();
+    sinon.stub(Validations, 'validateBook').returns();
 
     const { id, ...sendData } = book;
 
     const { status, body } = await chai
       .request(app)
-      .post("/books")
-      .set("authorization", "validToken")
+      .post('/books')
+      .set('authorization', 'validToken')
       .send(sendData);
 
     expect(status).to.equal(201);
     expect(body).to.deep.equal(book);
   });
 
-  it("shouldn't create a book without a token", async function () {
-    const { status, body } = await chai.request(app).post("/books");
+  it('shouldn\'t create a book without a token', async function() {
+    const { status, body } = await chai.request(app).post('/books');
 
     expect(status).to.equal(500);
-    expect(body.message).to.equal("Token not found");
+    expect(body.message).to.equal('Token not found');
   });
 
-  it("shouldn't create a book with an invalid token", async function () {
+  it('shouldn\'t create a book with an invalid token', async function() {
     const { status, body } = await chai
       .request(app)
-      .post("/books")
-      .set("authorization", "invalidToken");
+      .post('/books')
+      .set('authorization', 'invalidToken');
 
     expect(status).to.equal(500);
-    expect(body.message).to.equal("Token must be a valid token");
+    expect(body.message).to.equal('Token must be a valid token');
   });
 
-  it("shouldn't create a book with invalid body data", async function () {
-    sinon.stub(JWT, "verify").resolves();
+  it('shouldn\'t create a book with invalid body data', async function() {
+    sinon.stub(JWT, 'verify').resolves();
 
     const { status, body } = await chai
       .request(app)
-      .post("/books")
-      .set("authorization", "validToken")
+      .post('/books')
+      .set('authorization', 'validToken')
       .send({});
 
     expect(status).to.equal(400);
-    expect(body.message).to.equal("title is required");
+    expect(body.message).to.equal('title is required');
   });
 
-  it("should update a book", async function () {
-    sinon.stub(SequelizeBook, "update").resolves([1] as any);
-    sinon.stub(SequelizeBook, "findByPk").resolves(book as any);
-    sinon.stub(JWT, "verify").resolves();
-    sinon.stub(Validations, "validateBook").returns();
+  it('should update a book', async function() {
+    sinon.stub(SequelizeBook, 'update').resolves([1] as any);
+    sinon.stub(SequelizeBook, 'findByPk').resolves(book as any);
+    sinon.stub(JWT, 'verify').resolves();
+    sinon.stub(Validations, 'validateBook').returns();
 
     const { id, ...sendData } = book;
 
     const { status, body } = await chai
       .request(app)
-      .put("/books/1")
-      .set("authorization", "validToken")
+      .put('/books/1')
+      .set('authorization', 'validToken')
       .send(sendData);
 
     expect(status).to.equal(200);
-    expect(body.message).to.equal("Book updated");
+    expect(body.message).to.equal('Book updated');
   });
 
-  it("should return not found when the book to update does not exists", async function () {
-    sinon.stub(JWT, "verify").resolves();
-    sinon.stub(SequelizeBook, "findByPk").resolves(null);
+  it('should return not found when the book to update does not exists', async function() {
+    sinon.stub(JWT, 'verify').resolves();
+    sinon.stub(SequelizeBook, 'findByPk').resolves(null);
 
     const { id, ...sendData } = book;
 
     const { status, body } = await chai
       .request(app)
-      .put("/books/1")
-      .set("authorization", "validToken")
+      .put('/books/1')
+      .set('authorization', 'validToken')
       .send(sendData);
 
     expect(status).to.equal(404);
-    expect(body.message).to.equal("Book 1 not found");
+    expect(body.message).to.equal('Book 1 not found');
   });
 
-  it("should return conflict when there is nothing to be updated", async function () {
-    sinon.stub(JWT, "verify").resolves();
-    sinon.stub(SequelizeBook, "findByPk").resolves(book as any);
-    sinon.stub(SequelizeBook, "update").resolves([0] as any);
+  it('should return conflict when there is nothing to be updated', async function() {
+    sinon.stub(JWT, 'verify').resolves();
+    sinon.stub(SequelizeBook, 'findByPk').resolves(book as any);
+    sinon.stub(SequelizeBook, 'update').resolves([0] as any);
 
     const { id, ...sendData } = book;
 
     const { status, body } = await chai
       .request(app)
-      .put("/books/1")
-      .set("authorization", "validToken")
+      .put('/books/1')
+      .set('authorization', 'validToken')
       .send(sendData);
 
     expect(status).to.equal(409);
-    expect(body.message).to.equal("There are no updates to perform in Book 1");
+    expect(body.message).to.equal('There are no updates to perform in Book 1');
   });
 
-  it("should delete a book", async function () {
-    sinon.stub(SequelizeBook, "destroy").resolves();
-    sinon.stub(SequelizeBook, "findByPk").resolves(book as any);
-    sinon.stub(JWT, "verify").resolves();
+  it('should delete a book', async function() {
+    sinon.stub(SequelizeBook, 'destroy').resolves();
+    sinon.stub(SequelizeBook, 'findByPk').resolves(book as any);
+    sinon.stub(JWT, 'verify').resolves();
 
     const { status, body } = await chai
       .request(app)
-      .delete("/books/1")
-      .set("authorization", "validToken");
+      .delete('/books/1')
+      .set('authorization', 'validToken');
 
     expect(status).to.equal(200);
-    expect(body.message).to.equal("Book deleted");
+    expect(body.message).to.equal('Book deleted');
   });
 
-  it("should return not found when the book to delete does not exists", async function () {
-    sinon.stub(SequelizeBook, "findByPk").resolves(null);
-    sinon.stub(JWT, "verify").resolves();
+  it('should return not found when the book to delete does not exists', async function() {
+    sinon.stub(SequelizeBook, 'findByPk').resolves(null);
+    sinon.stub(JWT, 'verify').resolves();
 
     const { status, body } = await chai
       .request(app)
-      .delete("/books/1")
-      .set("authorization", "validToken");
+      .delete('/books/1')
+      .set('authorization', 'validToken');
 
     expect(status).to.equal(404);
-    expect(body.message).to.equal("Book 1 not found");
+    expect(body.message).to.equal('Book 1 not found');
   });
 
-  it("should change a book price", async function () {
-    sinon.stub(SequelizeBook, "update").resolves([1] as any);
-    sinon.stub(SequelizeBook, "findByPk").resolves(book as any);
-    sinon.stub(JWT, "verify").resolves();
+  it('should change a book price', async function() {
+    sinon.stub(SequelizeBook, 'update').resolves([1] as any);
+    sinon.stub(SequelizeBook, 'findByPk').resolves(book as any);
+    sinon.stub(JWT, 'verify').resolves();
 
     const { status, body } = await chai
       .request(app)
-      .patch("/books/1/discount")
-      .set("authorization", "validToken")
-      .send({ discount: "5" });
+      .patch('/books/1/discount')
+      .set('authorization', 'validToken')
+      .send({ discount: '5' });
 
     expect(status).to.equal(200);
-    expect(body.message).to.equal("Book updated");
+    expect(body.message).to.equal('Book updated');
   });
 
-  it("should return not found when the book to discount does not exists", async function () {
-    sinon.stub(SequelizeBook, "findByPk").resolves(null);
-    sinon.stub(JWT, "verify").resolves();
+  it('should return not found when the book to discount does not exists', async function() {
+    sinon.stub(SequelizeBook, 'findByPk').resolves(null);
+    sinon.stub(JWT, 'verify').resolves();
 
     const { status, body } = await chai
       .request(app)
-      .patch("/books/1/discount")
-      .set("authorization", "validToken")
-      .send({ discount: "5" });
+      .patch('/books/1/discount')
+      .set('authorization', 'validToken')
+      .send({ discount: '5' });
 
     expect(status).to.equal(404);
-    expect(body.message).to.equal("Book 1 not found");
+    expect(body.message).to.equal('Book 1 not found');
   });
 
-  it("should return a book by author", async function () {
-    sinon.stub(SequelizeBook, "findAll").resolves(books as any);
+  it('should return a book by author', async function() {
+    sinon.stub(SequelizeBook, 'findAll').resolves(books as any);
 
     const { status, body } = await chai
       .request(app)
-      .get("/books/author/search?q=Author");
+      .get('/books/author/search?q=Author');
 
     expect(status).to.equal(200);
     expect(body).to.deep.equal(books);
   });
 
-  it("should return not found when there is no books by author", async function () {
-    sinon.stub(SequelizeBook, "findAll").resolves([] as any);
+  it('should return not found when there is no books by author', async function() {
+    sinon.stub(SequelizeBook, 'findAll').resolves([] as any);
 
     const { status, body } = await chai
       .request(app)
-      .get("/books/author/search?q=Jon");
+      .get('/books/author/search?q=Jon');
 
     expect(status).to.equal(404);
-    expect(body.message).to.equal("Author Jon not found");
+    expect(body.message).to.equal('Author Jon not found');
   });
 
   afterEach(sinon.restore);
--- tests/mocks/Book.mocks.ts
+++ tests/mocks/Book.mocks.ts
@@ -1,8 +1,8 @@
 const book = { id: 1, title: 'Senhor dos Anéis', price: 22, author: 'Sei não', isbn: '234234' };
 
-const books = [book]
+const books = [book];
 
 export {
   book,
-  books
-}
+  books,
+};
